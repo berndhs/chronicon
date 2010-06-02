04eb94a6ed7a0e9b48861cd7136f10f049fd8959
Bernd Stramm <bernd.stramm@gmail.com>
2010-06-02 12:39:23 -0400

    almost ready to ship 0.1.4

--- fix_whirlpool.sh
+++ fix_whirlpool.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+#
+# This script disables some code in qca-ossl.cpp that will not build
+# with some old versions of openssl. 
+#
+# Only use this if you have problems compiling qca-ossl 
+# related to EVP_whirlpool()
+# 
 
 sed -e 's/OBJ_whirlpool$/OBJ_whirlpool_dont_use/g' qca-ossl.cpp > tmp.cpp
 mv tmp.cpp qca-ossl.cpp
