4a6b6f2cb6d0a9a1d0272fdcfd50ba4ba9d63aeb
Bernd Stramm <bernd.stramm@gmail.com>
2010-08-05 21:44:54 -0400

    packaging utility

---
+++ pack.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+NAME=chronicon
+VERSION=0.2.2
+PACKDIR=${HOME}/packaging/chronicon
+
+makearchive.sh ${NAME}-${VERSION} master
+cp ${NAME}-${VERSION}.tar.gz ${PACKDIR}
+echo ${NAME} > ${PACKDIR}/pack-name
+echo ${VERSION} > ${PACKDIR}/pack-version
+ls -l ${PACKDIR}/${NAME}-${VERSION}.tar.gz
+
+if [ x$1 == "xmake" ]
+then
+  cd ${PACKDIR}
+  make
+fi
